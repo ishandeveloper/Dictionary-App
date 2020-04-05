d4d531ede1bc579a7f3996aaf25992fd17a6a00f
Ishan Sharma <ishandeveloper@outlook.com>
2020-04-05 17:21:48 +0530

    Main

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Dictionary',
       theme: ThemeData(
-        primarySwatch: Colors.red
+        primarySwatch: Colors.blueGrey
       ),
       home: Homepage(),
     );
